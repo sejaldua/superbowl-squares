5f3bf71911680b3ae0fb6375b8b5a5c07b2c799e
Sejal Dua <sejaldua@gmail.com>
2023-02-11 14:14:38 -0800

    add color coding

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ th_props = [
 td_props = [
   ('font-size', '14px'),
   ('text-align', 'center'),
-  ('background-color', '#ffffff'),
+#   ('background-color', '#ffffff'),
   ('color', '#000')
   ]
                                  
@@ -27,13 +27,34 @@ styles = [
   dict(selector="td", props=td_props)
   ]
 
+color_dict = {
+    
+	'Pushpa'		: '#97C1A9',
+	'HDL'		    : '#B7CFB7',
+	'Bhupesh'		: '#55CBCD',
+	'Sarita'		: '#A3E1DC',
+	'Sejal'		    : '#B5EAD6',
+	'Rahul'		    : '#C7DBDA',
+	'Ameet'		    : '#FFB8B1',
+	'Eka'		    : '#FFDAC1',
+	'Sanvi'		    : '#F4978E',
+	'Sia'		    : '#F6EAC2',
+	'Ace'		    : '#FFE1E9',
+	'Sunil'		    : '#9AB7D3',
+	'Abs'		    : '#F5D2D3',
+	'Jayden'		: '#A2D2FF',
+	'Milen'		    : '#DFCCF1',
+	'Rowen'		    : '#BDE0FE',
+}
 
 # df = pd.read_csv('superbowl_squares.csv', index_col='Unnamed: 0')
-df = pd.read_excel('superbowl_squares.xlsx', index_col=[0,1], header=[0,1])  
-df = df.reset_index().fillna(method='ffill', axis=0).set_index('level_0')
-print(df.columns)
+df = pd.read_excel('superbowl_squares.xlsx', index_col=[0,1], header=[0,1]) 
+df = df.reset_index().fillna(method='ffill', axis=0).set_index(['level_0', 'level_1'])
+print(df.index) 
 df.columns = pd.MultiIndex.from_tuples([(col[0], col[1]) if col[0] != 'level_1' else ('', col[1]) for col in df.columns])
 for col in df.columns:
     df[col] = df[col].replace("Hanuman", "HDL")
+s = df.style.set_table_styles(styles)
 # st.dataframe(df.style.apply(lambda x: "background-color: red"))
-st.table(df.style.set_table_styles(styles))
+s2 = s.applymap(lambda v: f'background-color: {color_dict[v]};')
+st.table(s2)
