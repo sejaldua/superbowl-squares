903c4cbba00e3eb71c12c1fc231e3d2e72dd9ba1
Sejal Dua <sejaldua@gmail.com>
2024-02-04 09:48:56 -0500

    working optimizer and app

--- app.py
+++ app.py
@@ -3,9 +3,26 @@ import pandas as pd
 st.set_page_config(layout="wide")
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from optimizer import *
 
-st.title("Lahoti Srivastava Dua Superbowl Squares :football:")
-st.write("Hi family! Welcome to superbowl squares! The grid has been made using some randomization code to accommodate the number of squares that everyone has requested.")
+st.title("Superbowl Squares Generator :football:")
+st.caption("""
+The intent of this application is to alleviate the burden of making a Super Bowl squares grid for groups of 
+people that are engaging in some friendly betting fun! The program has been made using some linear optimization 
+code to accommodate the number of squares that everyone has requested.
+""")
+
+mode = st.sidebar.selectbox('Please select how you would like to enter your participant information', ['Manual Entry', 'Information Upload'])
+players = []
+squares = []
+if mode == 'Manual Entry':
+  num_participants = st.sidebar.number_input('How many players do you have?', value=None, min_value=2, max_value=100)
+  if num_participants is not None:
+    col1, col2 = st.sidebar.columns(2)
+    for i in range(num_participants):
+      players.append(col1.text_input(f'Player {i+1}'))
+      squares.append(col2.number_input(f'P{i+1} Squares', value=100//num_participants, min_value=1, max_value=99))
+print(players)
 
 # style
 th_props = [
@@ -14,6 +31,7 @@ th_props = [
   ('font-weight', 'bold'),
   ('color', '#6d6d6d'),
   ('background-color', '#f7ffff'),
+  ('width', '100px'),
    ('font-family', ' Avenir Medium'),
   ]
                                
@@ -31,25 +49,30 @@ styles = [
   dict(selector="td", props=td_props)
   ]
 
-color_dict = {
+palette = ['#FDD8B1', '#FFE5DE', '#F8BBD0', '#C1F0F3', '#DEF5F5', '#DCDCFE', '#EAF2D3', '#FC86A1', '#87CEEB', '#BDF7D3',
+'#ADBCA5', '#EAD637', '#A2D3C2', '#A499B3', '#9183EC', '#4CBDBB', '#F58266', '#38AECC', '#E5F98B']
+color_dict = {p: v for p, v in zip(players, palette[:len(players)])}
+color_dict['X'] = '#FFFFFF'
+print(color_dict)
+# color_dict = {
     
-	'Pushpa'		: '#97C1A9',
-	'HDL'		    : '#B7CFB7',
-	'Bhupesh'		: '#55CBCD',
-	'Sarita'		: '#A3E1DC',
-	'Sejal'		    : '#B5EAD6',
-	'Rahul'		    : '#C7DBDA',
-	'Ameet'		    : '#FFB8B1',
-	'Eka'		    : '#FFDAC1',
-	'Sanvi'		    : '#F4978E',
-	'Sia'		    : '#F6EAC2',
-	'Ace'		    : '#BCA89F',
-	'Sunil'		    : '#9AB7D3',
-	'Abs'		    : '#F5D2D3',
-	'Jayden'		: '#A2D2FF',
-	'Milen'		    : '#DFCCF1',
-	'Rowen'		    : '#BDE0FE',
-}
+# 	'Pushpa'		: '#97C1A9',
+# 	'Hanuman Das Lahoti'		    : '#B7CFB7',
+# 	'Bhupesh'		: '#55CBCD',
+# 	'Sarita'		: '#A3E1DC',
+# 	'Sejal'		    : '#B5EAD6',
+# 	'Rahul'		    : '#C7DBDA',
+# 	'Ameet'		    : '#FFB8B1',
+# 	'Eka'		    : '#FFDAC1',
+# 	'Sanvi'		    : '#F4978E',
+# 	'Sia'		    : '#F6EAC2',
+# 	# 'Ace'		    : '#BCA89F',
+# 	'Sunil'		    : '#9AB7D3',
+# 	'Abs'		    : '#F5D2D3',
+# 	'Jayden'		: '#A2D2FF',
+# 	'Milen'		    : '#DFCCF1',
+# 	'Rowen'		    : '#BDE0FE',
+# }
 
 def get_current_score():
   url = 'https://www.pro-football-reference.com/boxscores/202302120phi.htm'
@@ -60,7 +83,7 @@ def get_current_score():
     soup = BeautifulSoup(html)
     scorebox = soup.find("div", class_="scorebox")
     scores = scorebox.find_all("div", class_="score")
-    teams = ['Philadelphia Eagles', 'Kansas City Chiefs']
+    teams = ['San Francisco 49ers', 'Kansas City Chiefs']
     live_scores = [score.text for score in scores][::-1]
     return {t: int(s) for t, s in zip(teams, live_scores)}
   except:
@@ -76,6 +99,7 @@ def get_possible_scores(score):
 
 def style_specific_cell(x, outcomes):
     index1, index2 = outcomes.values()
+    print("outcomes", outcomes)
     curr_color = 'border: 2px solid black;'
     df1 = pd.DataFrame('', index=x.index, columns=x.columns)
     df1.loc[index1[0], index2[0]] = curr_color
@@ -85,21 +109,25 @@ def style_specific_cell(x, outcomes):
     return df1
 
 # df = pd.read_csv('superbowl_squares.csv', index_col='Unnamed: 0')
-df = pd.read_excel('superbowl_squares.xlsx', index_col=[0,1], header=[0,1]) 
-df = df.reset_index().fillna(method='ffill', axis=0).set_index(['level_0', 'level_1'])
-print(df.index) 
-df.columns = pd.MultiIndex.from_tuples([(col[0], col[1]) if col[0] != 'level_1' else ('', col[1]) for col in df.columns])
-for col in df.columns:
-    df[col] = df[col].replace("Hanuman", "HDL")
-s = df.style.set_table_styles(styles)
-# st.dataframe(df.style.apply(lambda x: "border-color: red" if ))
-s2 = s.applymap(lambda v: f'background-color: {color_dict[v]};')
-gamescore = get_current_score()
-if gamescore != {}:
-  print(gamescore)
-  indices = get_possible_scores(gamescore)
-  print(indices)
-  s3 = s.apply(lambda x: style_specific_cell(x, indices), axis=None)
-  st.table(s3)
-else:
-  st.table(s2)
+# df = pd.read_excel('superbowl_squares.xlsx', index_col=[0,1], header=[0,1]) 
+if st.sidebar.button('Generate Squares', use_container_width=True, type="primary"):
+  matrix = get_squares(players, squares)
+  df = pd.DataFrame(matrix, columns = list(range(10)))
+  df.index = [f'SF {x}' for x in df.index]
+  df.columns = [f'KC {x}' for x in df.columns]
+  print(df)
+  # s2 = pd.concat({'Foo': df}, names=['Firstlevel'])
+  s = df.style.applymap(lambda v: f'background-color: {color_dict[v]};').set_table_styles(styles)
+
+  # print(s)
+  # st.dataframe(df.style.apply(lambda x: "border-color: red" if ))
+  # s2 = s.applymap(lambda v: f'background-color: {color_dict[v]};')
+  # gamescore = get_current_score()
+  # if gamescore != {}:
+  #   print(gamescore)
+  #   indices = get_possible_scores(gamescore)
+  #   print(indices)
+  #   s3 = s.apply(lambda x: style_specific_cell(x, indices), axis=None)
+  #   st.table(s3)
+  # else:
+  st.dataframe(s, use_container_width=True)
--- optimizer.py
+++ optimizer.py
@@ -1,78 +1,69 @@
 from pulp import *
 
-matrix =  [[""]*10]*10
-print(matrix)
+def get_squares(Players, Squares):
+    print(sum(Squares))
+    if sum(Squares) < 100:
+        Players.append('X')
+        Squares.append(100-sum(Squares))
+    squares_per_player = {k: v for k, v in zip(Players, Squares)}
 
-# print sudoku problem
-for row in matrix:
-    for i, cell in enumerate(row):
-        if i < len(row) - 1:
-            print(cell, end=" ")
-        else:
-            print(cell)
-    print()
-print("+-------+-------+-------+")
+    Sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(Players)
 
-Players = [chr(65+i) for i in range(11)]
-Squares = [14, 4, 9, 9, 9, 9, 9, 9, 9, 9, 9]
-print(sum(Squares))
-if sum(Squares) < 100:
-    Players.append('X')
-    Squares.append(100-sum(Squares))
-squares_per_player = {k: v for k, v in zip(Players, Squares)}
+    # The Vals, Rows and Cols sequences all follow this form
+    Vals = Players
+    Rows = Sequence
+    Cols = Sequence
+    
+    # Define Problem       
+    prob = LpProblem("SuperBowl Problem",LpMinimize)
 
-Sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(Players)
+    # Creating a Set of Variables
+    choices = LpVariable.dicts("Choice",(Vals,Rows,Cols),0,1,LpInteger)
+    # print(choices)
 
-# The Vals, Rows and Cols sequences all follow this form
-Vals = Players
-Rows = Sequence
-Cols = Sequence
- 
-# Define Problem       
-prob = LpProblem("SuperBowl Problem",LpMinimize)
+    # Added arbitrary objective function
+    prob += 0, "Arbitrary Objective Function"
 
-# Creating a Set of Variables
-choices = LpVariable.dicts("Choice",(Vals,Rows,Cols),0,1,LpInteger)
-# print(choices)
-
-# Added arbitrary objective function
-prob += 0, "Arbitrary Objective Function"
-
-# A constraint ensuring that only one value can be in each square is created
-for r in Rows:
-    for c in Cols:
-        prob += lpSum([choices[v][r][c] for v in Vals]) == 1, ""
-
-# Add constraints that says there can only be minimum number of squares per player in each row or column
-for v in Vals:
+    # A constraint ensuring that only one value can be in each square is created
     for r in Rows:
-        prob += lpSum([choices[v][r][c] for c in Cols]) <= (squares_per_player[v] // 10) + 1, ""
-        
-    for c in Cols:
-        prob += lpSum([choices[v][r][c] for r in Rows]) <= (squares_per_player[v] // 10) + 1, ""
+        for c in Cols:
+            prob += lpSum([choices[v][r][c] for v in Vals]) == 1, ""
 
-    print(v, squares_per_player[v])
-    prob += lpSum([[choices[v][r][c] for r in Rows] for c in Cols]) == squares_per_player[v], ""
+    # Add constraints that says there can only be minimum number of squares per player in each row or column
+    for v in Vals:
+        for r in Rows:
+            prob += lpSum([choices[v][r][c] for c in Cols]) <= (squares_per_player[v] // 10) + 1, ""
             
+        for c in Cols:
+            prob += lpSum([choices[v][r][c] for r in Rows]) <= (squares_per_player[v] // 10) + 1, ""
 
-# Add constraints to minimize duplicates in rows and columns
-prob += lpSum([[[choices[v][r][c] + choices[v][r][c2] for c in Cols for c2 in Cols if c != c2] for r in Rows] for v in Vals])  # Minimize duplicates in rows
-prob += lpSum([[[choices[v][r][c] + choices[v][r2][c] for r in Rows for r2 in Rows if r != r2] for c in Cols] for v in Vals])  # Minimize duplicates in columns
+        print(v, squares_per_player[v])
+        prob += lpSum([[choices[v][r][c] for r in Rows] for c in Cols]) == squares_per_player[v], ""
+                
+
+    # Add constraints to minimize duplicates in rows and columns
+    prob += lpSum([[[choices[v][r][c] + choices[v][r][c2] for c in Cols for c2 in Cols if c != c2] for r in Rows] for v in Vals])  # Minimize duplicates in rows
+    prob += lpSum([[[choices[v][r][c] + choices[v][r2][c] for r in Rows for r2 in Rows if r != r2] for c in Cols] for v in Vals])  # Minimize duplicates in columns
 
 
-# The problem is solved using PuLP's choice of Solver
-prob.solve()
+    # The problem is solved using PuLP's choice of Solver
+    prob.solve()
 
-# The status of the solution is printed to the screen
-print("Status:", LpStatus[prob.status])
+    # The status of the solution is printed to the screen
+    # print("Status:", LpStatus[prob.status])
 
-# print out sudoku solution
-print("\nSuper Bowl Squares Solution")
-for r in Rows:
-    for c in Cols:
-        for v in Vals:
-            if choices[v][r][c].varValue == 1:               
-                print(v, end = " ")
-    print("")
-print("+-------+-------+-------+")
+    # print out sudoku solution
+    # print("\nSuper Bowl Squares Solution")
+    matrix = []
+    for r in Rows:
+        row = []
+        for c in Cols:
+            for v in Vals:
+                if choices[v][r][c].varValue == 1:   
+                    row.append(v)            
+                    print(v, end = " ")
+        matrix.append(row)
+        # print("")
+    # print("+-------+-------+-------+")
+    return matrix
